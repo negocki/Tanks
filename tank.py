1584135c626fce412f424ef5afae3b45498735d6
Karol Talko <katalko@gmail.com>
2017-04-08 16:24:55 +0200

    width, height, todo

--- tank.py
+++ tank.py
@@ -1,23 +1,25 @@
 import numpy as np
 class Level:
-    def __init__(self,file): #opening file with map and loading it to 2D lists
+    def __init__(self,file): #opening file with map and loading it to 2D list
+        width = 16
+        height = 10
         self.map = []
-        for i in range(0,10):
+        for i in range(0,height):
             x = []
-            for j in range(0,16):
+            for j in range(0,width):
                 x.append(0)
             self.map.append(x)
         fmap = open(file,'r')
         print("Wczytuje ",file)
         readdata = fmap.read()
         #print(readdata)
+        #TODO load from file to array
 
-        
         fmap.close()
 
+    #TODO displaying map in console
 
-
-
+#TODO game mechanics
 
 
 

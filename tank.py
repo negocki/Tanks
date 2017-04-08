9bc361cb8521da6db95c4e98cb0b9d16bab85efa
Karol Talko <katalko@gmail.com>
2017-04-08 16:51:55 +0200

    printing level in console(3 different tile types)

--- tank.py
+++ tank.py
@@ -1,30 +1,44 @@
 import numpy as np
 class Level:
+    width = 16
+    height = 10
     def __init__(self,file): #opening file with map and loading it to 2D list
-        width = 16
-        height = 10
         self.map = []
-        for i in range(0,height):
+        for i in range(0,self.height):
             x = []
-            for j in range(0,width):
+            for j in range(0,self.width):
                 x.append(0)
             self.map.append(x)
         fmap = open(file,'r')
         print("Wczytuje ",file)
-        for i in range(0,height):
+        for i in range(0,self.height):
             line = fmap.readline()
             #print(line)
-            for j in range(0,width):
+            for j in range(0,self.width):
                 self.map[i][j] = int(line[j]) #string from file to int in list
         fmap.close()
 
+
+    def display_map(self):
+        for i in range(0,self.height):
+            if(i%2==0):
+                print(" ",end="")
+            for j in range(0,self.width):
+                if(self.map[i][j]==0):
+                    print(".", end=" ") #empty field
+                elif(self.map[i][j]==1):
+                    print("#",end=" ") #obstacle
+                else:
+                    print("%",end=" ") #destructible obstacle
+            print()
     #TODO displaying map in console
 
-#TODO game mechanics
+
+# TODO game mechanics
 
 
 
 
 if(__name__ == "__main__"):
     mapka = Level("map1.txt")
-    print(mapka.map)
+    mapka.display_map()
